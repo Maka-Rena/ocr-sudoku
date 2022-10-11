9093abf1b4bb44c740727e7ca55cdf508013a1e7
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-10-11 13:53:45 +0200

    Adding the includes

--- grid_analysis/main.c
+++ grid_analysis/main.c
@@ -1,9 +1,8 @@
 #include <err.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "./include/parser.h"
-#include "./include/display.h"
-#include "./include/solver.h"
+#include "./include/to_matrix.h"
+#include "./include/cell.h"
 
 int main(int argc, char** argv)
 {
