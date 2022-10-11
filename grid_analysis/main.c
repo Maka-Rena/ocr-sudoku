84e7f30567c5b58e2eca7c4ba3e8489903efb5d3
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-10-11 13:20:12 +0200

    skeleton of the grid_analysis

---
+++ grid_analysis/main.c
@@ -0,0 +1,11 @@
+#include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "./include/parser.h"
+#include "./include/display.h"
+#include "./include/solver.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
