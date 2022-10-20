fd0c1bbafcd9a2f38b51028b9b014136c9fd119d
Khaled MILI <khaled.mili@epita.fr>
2022-10-20 23:19:10 +0200

    add training

--- neural_network/xor_recog_nn/main_xor.c
+++ neural_network/xor_recog_nn/main_xor.c
@@ -17,6 +17,14 @@ double sigmoid(double x)
     return 1.0 / (1 + exp(-1 * x)); 
 }
 
+/* Derivative of sigmoid function for backpropagation purposes
+ * x is the input
+ * return value of backpropagation*/
+double dsigmoid(double x)
+{
+    return x * (1 - x);
+}
+
 /* Function to initialize weights and biases with random values */
 double rand_number(){return ((double)rand())/((double)RAND_MAX);}
 
@@ -40,48 +48,59 @@ int main()
     //matrix_print(inputs);
     //matrix_print(expected_outputs);
 
-    //Now we initialize the weights and biases with random values
+    //Initializing hidden weights biases with random values
     Matrix *hidden_weights = matrix_create(inputLayerNeurons,hiddenLayerNeurons);//OK
-    //two for loops to fill the matrix with random numbers 
-    //IMPORTANT : NEED TO CHANGE IT TO FUNCTION AND PUT IT IN OPS.C
-    for(int i = 0; i < hidden_weights->rows; i++)
-        for(int j = 0; j < hidden_weights->cols; j++)
-            hidden_weights -> entries[i][j] = rand_number();
-    //matrix_print(hidden_weights);
+    matrix_randomize(hidden_weights,hiddenLayerNeurons);
+    matrix_print(hidden_weights);
     Matrix *hidden_biases = matrix_create(1,hiddenLayerNeurons);
-    for(int i = 0; i < hidden_biases->rows; i++)
-        for(int j = 0; j < hidden_biases->cols; j++)
-            hidden_biases->entries[i][j] = rand_number();
-    //matrix_print(hidden_biases);
+    matrix_randomize(hidden_biases,hiddenLayerNeurons);
+    matrix_print(hidden_biases);
+
+    //Initializing output weights and biases with random values
     Matrix *output_weights = matrix_create(hiddenLayerNeurons,outputLayerNeurons);//OK
-    for(int i = 0; i < output_weights->rows; i++)
-        for(int j = 0; j < output_weights->cols; j++)
-            output_weights->entries[i][j] = rand_number();
-    //matrix_print(output_weights);
-    Matrix *output_biases = matrix_create(1,outputLayerNeurons);//OK
-    //for the output_biases it is a matrix of n rows and 1 col
-    for(int i = 0; i < output_biases->rows; i++)
-        output_biases->entries[i][0] = rand_number();
+    matrix_randomize(output_weights,outputLayerNeurons);
+    matrix_print(output_weights);
+    Matrix *output_biases = matrix_create(1,outputLayerNeurons);
+    matrix_randomize(output_biases,outputLayerNeurons);
     matrix_print(output_biases);
 
-    //Now we move to the activation of the our layers
-    Matrix *hidden_layer_activation = dot(inputs,hidden_weights);
+    // Activation of hidden Layer
+    Matrix *hidden_layer_activation = dot(inputs,hidden_weights); // rows = 2 and cols = 4
     printf("hidden layer activation\n");
     matrix_print(hidden_layer_activation);
-    //matrix_print(hidden_biases);
-    hidden_layer_activation = add(hidden_layer_activation, hidden_biases); //PROBLEM : MISSMATCH IN ADDITION
-    //matrix_print(hidden_layer_activation);
-    //And we apply the sigmoid function on our activated hidden layer
-    Matrix *hidden_layer_output = matrix_create(hidden_layer_activation->rows,hidden_layer_activation->cols);
-    for(int i = 0; i < hidden_layer_output -> rows;i++)
-        for(int j = 0; j < hidden_layer_output -> cols; j++)
-            hidden_layer_output -> entries[i][j] = sigmoid(hidden_layer_output -> entries[0][i]);
-    //matrix_print(hidden_layer_output);
+    
+    //We add the two elements contained in hidden_biases to the corresponding
+    //two elements for each column in hidden_layer_activation
+    for(int j = 0; j < hidden_layer_activation->rows; j++)
+            for(int i = 0; i < hidden_biases -> cols ; i++)
+                hidden_layer_activation->entries[j][i] = hidden_layer_activation->entries[j][i] + hidden_biases->entries[0][i];
+    printf("new value of hidden_layer_activation\n");
+    matrix_print(hidden_layer_activation);
 
+    //We apply the sigmoid function to the hidden_layer_activation elements to
+    //get the hidden_layer_output
+    //It is important to initiate the matrix before associating it to a new
+    //matrix (the return of the function apply for example)
+    Matrix *hidden_layer_output = matrix_create(hidden_layer_activation->rows,hidden_layer_activation->cols);
+    hidden_layer_output = apply(sigmoid,hidden_layer_activation);
+    printf("hidden layer output\n");
+    matrix_print(hidden_layer_output);
+    
+    //We do the same for the output_layer_activation to get the predicted output
     Matrix *output_layer_activation = dot(hidden_layer_output, output_weights);
-    output_layer_activation = add(output_layer_activation,output_biases);
-    Matrix *predicted_output = matrix_create(1,1);
-    predicted_output -> entries[0][0] = sigmoid(output_layer_activation -> entries[0][0]);
+    printf("output_layer_activation :\n");
+    matrix_print(output_layer_activation);
+
+    for(int j = 0; j < output_layer_activation->rows; j++)
+        for(int i = 0; i < output_biases->cols; i++)
+            output_layer_activation->entries[j][i] = output_layer_activation->entries[j][i] + output_biases->entries[0][i];
+    printf("output_layer_activation :\n");
+    matrix_print(output_layer_activation);
+    Matrix *predicted_output = matrix_create(output_layer_activation->rows,output_layer_activation->cols);
+    predicted_output = apply(sigmoid,output_layer_activation);
+    printf("predicted_output :\n");
     matrix_print(predicted_output);
+
+    //BackPropagation:
 
 }
