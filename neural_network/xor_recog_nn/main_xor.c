4b5a575e57e4d9c335273ad8c8dd018af1508a7d
Khaled MILI <khaled.mili@epita.fr>
2022-10-23 00:35:29 +0200

    adjusting xor nn

--- neural_network/xor_recog_nn/main_xor.c
+++ neural_network/xor_recog_nn/main_xor.c
@@ -9,7 +9,7 @@
 #define hiddenLayerNeurons 2
 #define outputLayerNeurons 1
 #define epochs 100000
-#define learning_rate 0.2f
+#define learning_rate 0.4f
 /* Sigmoid function
  * x is the activation input
  * returns the activation value */
@@ -43,32 +43,32 @@ double rand_number(){return ((double)rand())/((double)RAND_MAX);}
 
 int main()
 {
-    Matrix *inputs = matrix_create(4,2);
+    Matrix *inputs = matrix_create(2,4);
     //all the values of the matrix are initialized to 0.0f
     //so we only need to fill with ones (following XOR table)
 
     inputs -> entries[1][1] = 1.0f;
-    inputs -> entries[2][0] = 1.0f;
-    inputs -> entries[3][0] = 1.0f;
-    inputs -> entries[3][1] = 1.0f;
+    inputs -> entries[0][2] = 1.0f;
+    inputs -> entries[0][3] = 1.0f;
+    inputs -> entries[1][3] = 1.0f;
 
 
-    Matrix *expected_output = matrix_create(4,1);
-    expected_output -> entries[1][0] = 1.0f;
-    expected_output -> entries[2][0] = 1.0f;
+    Matrix *expected_output = matrix_create(1,4);
+    expected_output -> entries[0][1] = 1.0f;
+    expected_output -> entries[0][2] = 1.0f;
 
     //matrix_print(inputs);
     //matrix_print(expected_output);
 
     //Initializing hidden weights biases with random values
-    Matrix *hidden_weights = matrix_create(inputLayerNeurons,hiddenLayerNeurons);//OK
-    matrix_randomize(hidden_weights,11);//2x2
+    Matrix *hidden_weights = matrix_create(hiddenLayerNeurons,inputLayerNeurons);//2x2
+    matrix_randomize(hidden_weights,11);
     /*Matrix *hidden_bias = matrix_create(1,hiddenLayerNeurons);
       matrix_randomize(hidden_bias,11);
       matrix_print(hidden_bias);*/
 
     //Initializing output weights with random values
-    Matrix *output_weights = matrix_create(hiddenLayerNeurons,outputLayerNeurons);//OK
+    Matrix *output_weights = matrix_create(outputLayerNeurons, hiddenLayerNeurons);//1x2
     matrix_randomize(output_weights,11);
     printf("output_weights :\n");
     matrix_print(output_weights);
@@ -87,7 +87,7 @@ int main()
     {
         //Forward Propagation
         // Activation of hidden Layer
-        Matrix *hidden_layer_input = dot(hidden_weights,transpose(inputs)); // 2x4
+        Matrix *hidden_layer_input = dot(hidden_weights,inputs); // 2x4
         //printf("hidden layer input\n");
         //matrix_print(hidden_layer_input);
 
@@ -98,7 +98,7 @@ int main()
         //matrix_print(hidden_layer_output);
 
         //We do the same for the output_layer_activation to get the predicted output
-        Matrix *final_input = dot(transpose(output_weights),hidden_layer_output);//1x4
+        Matrix *final_input = dot(output_weights,hidden_layer_output);//1x4
         //printf("final_input :\n");
         //matrix_print(final_input);
 
@@ -110,21 +110,21 @@ int main()
 
         //BackPropagation:
         //We first compute the error
-        Matrix* output_errors = substract(transpose(expected_output), final_output);//1x4
+        Matrix* output_errors = substract(expected_output, final_output);//1x4
         //printf("error :\n");
         //matrix_print(output_errors);
-        Matrix* hidden_errors = dot(output_weights,output_errors);//2x4
+        Matrix* hidden_errors = dot(transpose(output_weights),output_errors);//2x4
         //matrix_print(hidden_errors);
 
-        Matrix* sigmoid_primed_mat = sigmoidPrime(final_output);//4x1
+        Matrix* sigmoid_primed_mat = sigmoidPrime(final_output);//1x4
         Matrix* multiplied_mat = multiply(output_errors, sigmoid_primed_mat);//1x4
         Matrix* transposed_mat = transpose(hidden_layer_output);//4x2
         Matrix* dot_mat = dot(multiplied_mat, transposed_mat);//1x2
-        Matrix* scaled_mat = scale(learning_rate,dot_mat);
+        Matrix* scaled_mat = scale(learning_rate,dot_mat);//1x2
 
-        Matrix* added_mat = add(transpose(output_weights), scaled_mat);//2x1 + 2x1 but initially output_weights not transposed (?)
+        Matrix* added_mat = add(output_weights, scaled_mat);//1x2
         matrix_free(output_weights);
-        output_weights = transpose(added_mat);
+        output_weights = added_mat;
 
         // freeing memory to reuse the variables after
         matrix_free(sigmoid_primed_mat);
@@ -135,17 +135,17 @@ int main()
 
         sigmoid_primed_mat = sigmoidPrime(hidden_layer_output);//2x4
         multiplied_mat = multiply(hidden_errors, sigmoid_primed_mat);//2x4
-        //transposed_mat = transpose(inputs);
-        dot_mat = dot(multiplied_mat,inputs);//2x2
-        scaled_mat = scale(learning_rate, dot_mat);
-        added_mat = add(hidden_weights, scaled_mat);
+        transposed_mat = transpose(inputs);//4x2
+        dot_mat = dot(multiplied_mat,transposed_mat);//2x2
+        scaled_mat = scale(learning_rate, dot_mat);//2x2
+        added_mat = add(hidden_weights, scaled_mat);//2x2
         matrix_free(hidden_weights);
         hidden_weights = added_mat;
 
         //freeing memory at the end
         matrix_free(sigmoid_primed_mat);
         matrix_free(multiplied_mat);
-        //matrix_free(transposed_mat);
+        matrix_free(transposed_mat);
         matrix_free(dot_mat);
         matrix_free(scaled_mat);
 
