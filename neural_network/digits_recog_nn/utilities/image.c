e113ba68c822c22ee0507c328f1b181eda3afd40
Khaled MILI <khaled.mili@epita.fr>
2022-10-24 00:57:44 +0200

    adjusting gitignore and finishing image functions

--- neural_network/digits_recog_nn/utilities/image.c
+++ neural_network/digits_recog_nn/utilities/image.c
@@ -5,6 +5,43 @@
 
 #define MAXCHAR 10000
 
+Image** csv_to_imgs(char* file_string, int number_of_imgs) 
+{
+    FILE *fp;
+    Image** imgs = malloc(number_of_imgs * sizeof(Image*));
+    char row[MAXCHAR];
+    fp = fopen(file_string, "r");
+
+    //Read the first line 
+    fgets(row, MAXCHAR, fp);
+    int i = 0;
+    while (feof(fp) != 1 && i < number_of_imgs)
+    {
+        imgs[i] = malloc(sizeof(Image));
+
+        int j = 0;
+        fgets(row, MAXCHAR, fp);
+        char* token = strtok(row, ",");
+        imgs[i]->img_data = matrix_create(28,28);
+        while(token != NULL)
+        {
+            if(j == 0)
+            {
+                imgs[i]->label = atoi(token);
+            }
+            else
+            {
+                imgs[i]->img_data->entries[(j-1)/28][(j-1)%28] = atoi(token)/256.0;
+            }
+            token = strtok(NULL,",");
+            j++;
+        }
+        i++;
+    }
+    fclose(fp);
+    return imgs;
+}
+
 void img_print(Image* img)
 {
     matrix_print(img->img_data);
