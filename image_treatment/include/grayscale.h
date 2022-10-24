2cb74f1528d553ac8620c2d3007cee4e7734746b
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-10-24 11:50:25 +0200

    Changing the architecture of the file Image_treatment according to the global one

---
+++ image_treatment/include/grayscale.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+void draw(SDL_Renderer* renderer, SDL_Texture* texture);
+
+void event_loop(SDL_Renderer* renderer, SDL_Texture* colored, SDL_Texture* texture_blurred);
+
+SDL_Surface* Load_image(const char* path);
+
+Uint32 __Pixel_To_Grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
+
+void Surface_To_Grayscale(SDL_Surface* surface);
