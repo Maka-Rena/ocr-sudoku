293c4b9cb30a0fd2a7cdbe759ed51f65f7b1c445
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-10-27 13:45:00 +0200

    first step of implementing the croped image

---
+++ sudoc/image_management/line_detection/include/crop.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
--- sudoc/image_management/line_detection/main_detection.c
+++ sudoc/image_management/line_detection/main_detection.c
@@ -5,45 +5,46 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+// Loads an image in a surface.
+// The format of the surface is SDL_PIXELFORMAT_RGB888.
+//
+// path: Path of the image.
+SDL_Surface* Load_image(const char* path)
+{
+    SDL_Surface* surface = IMG_Load(path);
+    if (surface == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    SDL_Surface* res = SDL_ConvertSurfaceFormat(surface,SDL_PIXELFORMAT_RGB888,0);
+    if (res == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    SDL_FreeSurface(surface);
+    return res;
+}
+
 int main(int argc, char** argv)
 {
     // Checks the number of arguments.
-    if (argc != 2)
-        errx(EXIT_FAILURE, "Usage: image-file");
+    if (argc != 3)
+        errx(EXIT_FAILURE, "Usage: int1 int2");
     
     // - Initialize the SDL.
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    // - Create a window.
-    SDL_Window* window = SDL_CreateWindow("Plain Window", 0, 0, 640, 400,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    // - Create a renderer.
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     // - Create a surface from the colored image.
     SDL_Surface* surface = Load_image(argv[1]);
     if (surface == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
+	
+	SDL_Surface* croped = SDL_CreateRGBSurface(0, argv[1], argv[2], 32,0,0,0,0);
+	SDL_FillRect(croped, NULL, SDL_MapRGB(rotated->format, 255, 255, 255));
+	
+	crop_image(surface,croped);
+	IMG_SavePNG(croped, "croped_picture.png");
 
-    // - Resize the window according to the size of the image.
-    SDL_SetWindowSize(window, surface->w, surface->h);
-    
-    // - Create a new texture from the surface.
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (texture == NULL)
-		  errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    
     // - Destroy the objects.
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_DestroyTexture(texture);
+	SDL_FreeSurface(surface);
+	SDL_FreeSurface(croped);
     SDL_Quit();
 
     return EXIT_SUCCESS;
