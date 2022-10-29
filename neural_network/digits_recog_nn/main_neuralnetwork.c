6bd7afef7c27610236d942c9cbbeb29c24578c84
Khaled MILI <khaled.mili@epita.fr>
2022-10-29 17:14:05 +0200

    fixing architecture

--- neural_network/digits_recog_nn/main_neuralnetwork.c
+++ neural_network/digits_recog_nn/main_neuralnetwork.c
@@ -5,13 +5,11 @@
 
 int main()
 {
-    NeuralNetwork* net = malloc(sizeof(NeuralNetwork));
-    net = network_load("baptisto");
-    printf("%i\n",net->input);
-    printf("%i\n",net->hidden);
-    printf("%i\n",net->output);
-    matrix_print(net->hidden_weights);
-    matrix_print(net->output_weights);
-    if(chdir("-")){}
-    network_save(net,"baptistou");
+    NeuralNetwork* o = network_load("to_load_dir");
+    //printf("%i\n",o->input);
+    //printf("%i\n",o->hidden);
+    //printf("%i\n",o->output);
+    //matrix_print(o->hidden_weights);
+    //matrix_print(o->output_weights);
+    network_save(o,"to_save_dir");
 }
