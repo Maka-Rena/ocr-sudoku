173854845642c5cc45a4be16c075d168baa5a2c0
Khaled MILI <khaled.mili@epita.fr>
2022-10-29 17:07:09 +0200

    load and save fixed completely

--- neural_network/digits_recog_nn/load_and_save.c
+++ neural_network/digits_recog_nn/load_and_save.c
@@ -4,7 +4,10 @@ NeuralNetwork* network_load(char* dir_name)
 {
     NeuralNetwork* net = malloc(sizeof(NeuralNetwork));
     // Go to the directory where the Neural Network was loaded
-    if(chdir(dir_name)){}
+    if(chdir(dir_name) == 0)
+    {
+        printf("Entered in %s\n",dir_name);
+    }
     FILE* descriptor = fopen("descriptor", "r");
     // Use of & to associate the value to the address of input
     int input;
@@ -20,12 +23,15 @@ NeuralNetwork* network_load(char* dir_name)
     fclose(descriptor);
     net->hidden_weights = matrix_load("hidden_weights");
     net->output_weights = matrix_load("output_weights");
-    /*Add later for the bias*/
+    //Add later for the bias
     //net->hidden_bias = matrix_load("hidden_bias");
     //net->output_bias = matrix_load("output_bias");
     printf("Network succesfully loaded\n");
     // Go back to the previous directory
-    if(chdir("-")){}
+    if(chdir("..") == 0)
+    {
+        printf("Returned to original directory\n");
+    }
     return net;
 }
 
@@ -34,7 +40,10 @@ void network_save(NeuralNetwork* net,char* dir_name)
     // Create a new directory
     mkdir(dir_name, 0777);
     // Go into the new directory
-    if(chdir(dir_name)){}
+    if(chdir(dir_name) == 0)
+    {
+        printf("Entered in new directory\n");
+    }
     // Create the descriptor file
     FILE* descriptor = fopen("descriptor", "w");
     fprintf(descriptor,"%i\n%i\n%i\n",net->input,net->hidden,net->output);
@@ -47,5 +56,8 @@ void network_save(NeuralNetwork* net,char* dir_name)
     //matrix_save(net->output_bias, "output_bias");
     printf("Network successfully saved\n");
     // Go back to the previous directory
-    if(chdir("-")){}
+    if(chdir("..") == 0)
+    {
+        printf("Returned to original directory\n");
+    }
 }
