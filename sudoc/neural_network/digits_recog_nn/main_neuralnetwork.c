5f801664087437c57b53704e3200c2d39c6d2427
Khaled MILI <khaled.mili@epita.fr>
2022-12-06 21:55:44 +0100

    adjustment and starting conversion from crop

--- sudoc/neural_network/digits_recog_nn/main_neuralnetwork.c
+++ sudoc/neural_network/digits_recog_nn/main_neuralnetwork.c
@@ -5,6 +5,49 @@
 #include <string.h>
 #include "include/neural_network.h"
 #include "include/load_and_save.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+Matrix *lower(SDL_Surface* surface)
+{
+  
+  Uint32* pixels = surface->pixels;
+  if (pixels == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+  //le format
+  SDL_PixelFormat* format = surface->format;
+    if (format == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+Matrix* res = matrix_create(28,28);
+  for (int i = 0;i < surface->h;i++)
+    {
+        for (int j =0; j < surface->w;j++)
+        { 
+            Uint8 r,g,b;
+            float av = ((float)(r+g+b))/3;
+            SDL_GetRGB(pixels[i*surface->w + j], format, &r, &g, &b);
+            //if(r == 255)
+                res->entries[i][j] = av;
+            //else
+                res->entries[i][j] = av;
+        }
+    }
+    return res;
+}
+
+SDL_Surface* Load_image(const char* path)
+{
+    SDL_Surface* surface = IMG_Load(path);
+    if (surface == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    SDL_Surface* res = SDL_ConvertSurfaceFormat(surface,SDL_PIXELFORMAT_RGB888,0);
+    if (res == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    SDL_FreeSurface(surface);
+    return res;
+}
 
 int main(int argc, char** argv)
 {
@@ -28,7 +71,6 @@ int main(int argc, char** argv)
     {
         int number_imgs = 10000;
         Image** imgs = csv_to_imgs("../../libraries/data_set_lib/DataSet/mnist_test.csv", number_imgs);
-        printf("ntmmmmm\n");
         NeuralNetwork* net = network_load("testing_net");
         printf("On est ok !\n");
         double score = network_predict_imgs(net, imgs, 10000);
@@ -38,10 +80,26 @@ int main(int argc, char** argv)
         network_free(net);
     }
 
-    /*matrix_print(imgs[5]->img_data); 
-      Matrix* res = network_predict_img(net,imgs[5]);
-      printf("predicted number = %i\n", matrix_argmax(res));
-      printf("actual number = %i\n", imgs[5]->label);
-      imgs_free(imgs, number_imgs);*/
+    if(strcmp((argv[1]), "test") == 0)
+    {
+        SDL_Surface *surface = Load_image("../../../pictures/28x28.png");
+        NeuralNetwork* net = network_load("testing_net");
+        Matrix *to_img = lower(surface);
+        
+        printf("heyho\n");
+        Image *my_img = malloc(sizeof(Image));
+        my_img->img_data = to_img;
+        my_img->label = 6;
+        printf("heyhey\n");
+        Matrix *res = network_predict_img(net,my_img);
+        printf("predicted number = %i\n", matrix_argmax(res));
+        printf("actual number = %i\n", 6);
+        network_free(net);
+        /*matrix_print(imgs[5]->img_data); 
+        Matrix* res = network_predict_img(net,imgs[5]);
+        printf("predicted number = %i\n", matrix_argmax(res));
+        printf("actual number = %i\n", imgs[5]->label);
+        imgs_free(imgs, number_imgs);*/
+    }
     return 0;
 }
