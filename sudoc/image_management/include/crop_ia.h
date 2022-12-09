bb5d45c4e4483d21b5289d1fa3e115b18acc6b3b
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2022-12-09 12:15:15 +0100

    crop function according to coordinates

--- sudoc/image_management/include/crop_ia.h
+++ sudoc/image_management/include/crop_ia.h
@@ -4,3 +4,7 @@
 #include <err.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "crop.h"
+#include "resize.h"
+
+void crop_9(SDL_Surface* surface, int* coordinates);
--- sudoc/image_management/libs/crop_ia.c
+++ sudoc/image_management/libs/crop_ia.c
@@ -1,1 +1,18 @@
 #include "../include/crop_ia.h"
+
+void crop_9(SDL_Surface* surface, int* coordinates)
+{
+    int length = sizeof(coordinates) / sizeof(int*);
+
+    for (int i = 0; i < length-1; i += 2)
+    {
+        SDL_Surface* cropped = SDL_CreateRGBSurface(0, surface->h, surface->w, 32, 0, 0, 0, 0);
+        croped_image(surface, cropped, coordinates[i], coordinates[i+1]);
+        SDL_Surface* resized = resize(cropped, 28, 28);
+        char* name = malloc(sizeof(char) * 5);
+        name = i + ".png";
+        IMG_SavePNG(resized, name);
+        free(resized);
+        free(cropped);
+    }
+}
