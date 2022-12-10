fd1b7cd032b61d08d51fb68fc4cf0df902c8b240
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2022-12-10 16:22:13 +0100

    remove an error

--- sudoc/image_management/libs/crop_ia.c
+++ sudoc/image_management/libs/crop_ia.c
@@ -4,10 +4,10 @@ void crop_9(SDL_Surface* surface, int* coordinates)
 {
     int length = sizeof(coordinates) / sizeof(int*);
 
-    for (int i = 0; i < length-1; i += 2)
+    for (int i = 0; i < length-3; i += 2)
     {
         SDL_Surface* cropped = SDL_CreateRGBSurface(0, surface->h, surface->w, 32, 0, 0, 0, 0);
-        croped_image(surface, cropped, coordinates[i], coordinates[i+1]);
+        croped_image(surface, cropped, coordinates[i], coordinates[i+1], coordinates[i+2], coordinates[i+3]);
         SDL_Surface* resized = resize(cropped, 28, 28);
         char* name = malloc(sizeof(char) * 5);
         name = i + ".png";
